d20912ab2b05399ba95ed987d1ef48ab13574d80
Shagun <sshagunsodhani@gmail.com>
2014-11-25 00:06:12 +0530

    seperated helper functions

---
+++ helper.py
@@ -0,0 +1,22 @@
+import sys
+from itertools import chain, combinations
+from collections import defaultdict
+from optparse import OptionParser
+
+
+def subsets(arr):
+    """ Returns non empty subsets of arr"""
+    return chain(*[combinations(arr, i + 1) for i, a in enumerate(arr)])
+
+
+def joinSet(itemSet, length):
+    """Join a set with itself and returns the n-element itemsets"""
+    return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])  
+
+def dataFromFile(fname):
+    """Function which reads from the file and yields a generator"""
+    file_iter = open(fname, 'rU')
+    for line in file_iter:
+        line = line.strip().rstrip(',')                         # Remove trailing comma
+        record = frozenset(line.split(','))
+        yield record
